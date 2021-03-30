19df03e10f11cf4d767c1f36f75493bb73473144
David Maier <david.maier@redislabs.com>
2021-03-30 22:55:57 +0200

    Minor change

--- run.bash
+++ run.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
 ## Redis
+echo "Starting Redis ..."
 redis-server --loadmodule /home/modules/redisai.so --loadmodule /home/modules/redisgears.so Plugin /home/modules/vector_similarity.so &
+sleep 5
 
 ## Flask
+echo "Starting Server ..."
 source ./env/bin/activate
 export FLASK_APP=server.py
 flask run

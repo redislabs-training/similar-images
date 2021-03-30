33cd9d0860c1cf5ad79217f8e0ec68037eaf7370
David Maier <david.maier@redislabs.com>
2021-03-30 22:29:08 +0200

    Added a run script

--- run.bash
+++ run.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 redis-server --loadmodule /home/modules/redisai.so --loadmodule /home/modules/redisgears.so Plugin /home/modules/vector_similarity.so &
-python /home/code/similar-images/server.py
+python3 /home/code/similar-images/server.py
